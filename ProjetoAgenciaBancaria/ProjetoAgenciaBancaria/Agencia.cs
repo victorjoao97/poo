bcc50d92c16e779083652f7978c037e105fdfbc3
João Victor Nascimento <victorjoao97@outlook.com>
2020-04-07 21:48:23 -0300

    criado a classe para instanciar dentro de outra classe

---
+++ ProjetoAgenciaBancaria/ProjetoAgenciaBancaria/Agencia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoAgenciaBancaria
+{
+    class Agencia
+    {
+        public string Numero { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+    }
+}
