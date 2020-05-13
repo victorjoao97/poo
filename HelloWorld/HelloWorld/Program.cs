0dad6405f94d1b67b75caa1c40b16e0197c3d2e4
João Victor Nascimento <victorjoao97@outlook.com>
2020-05-13 20:56:39 -0300

    primeiro projeto hello world

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            do
+            {
+                Console.Write("Digite seu nome: ");
+                nome = Console.ReadLine();
+                TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+                Console.WriteLine($"Bem vindo {myTI.ToTitleCase(nome)}");
+            } while (nome.Length <= 0);
+            Console.ReadKey();
+        }
+    }
+}
